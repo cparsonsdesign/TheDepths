a613f5fce58d34ccc7e20a0cdfe6d1c116ed53d8
Unknown <chris@cparsonsdesign.com>
2017-06-22 01:53:32 -0500

    Player weapons instantiate inthe proper position and rotation

--- Assets/Characters/PlayerCharacters/Player.cs
+++ Assets/Characters/PlayerCharacters/Player.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable {
 
+    #region(Player Stats)
     [SerializeField]
     private float currentHealth;
     [SerializeField]
@@ -11,13 +14,19 @@ public class Player : MonoBehaviour, IDamageable {
     [SerializeField]
     private int enemyLayer = 9;
     [SerializeField]
-    private int damagePerHit = 10;
+    Weapon weaponInUse;
+    #endregion
 
+
+    #region (For Attacking)
+    [SerializeField]
+    private int damagePerHit = 10;
     [SerializeField]
     private float minTimeBetweenHits = .5f;
     [SerializeField]
     private float maxAttackRange = 2f;
     private float lastHitTime = 0f;
+    #endregion
 
     GameObject currentTarget;
     CameraRaycaster camRay;
@@ -33,10 +42,36 @@ public class Player : MonoBehaviour, IDamageable {
 
     private void Start()
     {
+        RegisterForMouseClick();
+        currentHealth = maxHealth;
+        PutWeaponInHand();
+    }
+
+    private void PutWeaponInHand()
+    {
+        var weaponPrefab = weaponInUse.GetWeaponPrefab();
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+        weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+        weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.IsFalse(numberOfDominantHands <= 0, "No DomiantHand found, Please add one.");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove extras.");
+        return dominantHands[0].gameObject;
+        
+    }
+
+    private void RegisterForMouseClick()
+    {
         camRay = FindObjectOfType<CameraRaycaster>();
         camRay.notifyMouseClickObservers += OnMouseClicked;
-        currentHealth = maxHealth;
     }
+
     public void TakeDamage (float damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - damage, 0f, maxHealth);
